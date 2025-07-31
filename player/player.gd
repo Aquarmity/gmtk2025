d845d764040fe2cac1cb92e63dc206dd60a5c76f
Aquarmity <62366478+Aquarmity@users.noreply.github.com>
2025-07-31 08:54:18 -0700

    freezing works

--- player/player.gd
+++ player/player.gd
@@ -2,15 +2,17 @@ extends CharacterBody2D
 
 @onready var explosion_area = $ExplosionArea/CollisionShape2D
 @onready var timer = $Timer
+@onready var collision = $CollisionShape2D
+@onready var sprite = $Sprite2D
 
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -800.0
-var color: GlobalVars.PlayerColor
+var frozen_scene = preload("res://frozen_player/frozen_player.tscn")
+@export var color: GlobalVars.PlayerColor
 var spawnPos: Vector2
 var allow_input = true
 
 func _ready() -> void:
-	color = GlobalVars.PlayerColor.RED
 	spawnPos = position
 	explosion_area.disabled = true
 
@@ -36,22 +38,36 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("action"):
+	if event.is_action_pressed("action") and allow_input:
+		allow_input = false
+		velocity = Vector2(0, 0)
+		
 		match color:
 			GlobalVars.PlayerColor.RED:
 				explosion_area.disabled = false
-				allow_input = false
-				velocity = Vector2(0, 0)
-				timer.start()
 			GlobalVars.PlayerColor.GREEN:
 				pass
 			GlobalVars.PlayerColor.BLUE:
-				pass
+				collision.disabled = true
+				sprite.visible = false
+				var frozen_instance = frozen_scene.instantiate()
+				frozen_instance.position = position
+				get_parent().add_child(frozen_instance)
+				position = spawnPos
+				
+		timer.start()
+	
+	if event.is_action_pressed("retry"):
+		get_tree().reload_current_scene()
 
 func _on_timer_timeout() -> void:
 	position = spawnPos
 	explosion_area.disabled = true
 	allow_input = true
+	collision.disabled = false
+	sprite.visible = true
+	
+	color = (color + 1) % 3
 
 func set_color(c: GlobalVars.PlayerColor) -> void:
 	color = c
